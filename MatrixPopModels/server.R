1832940c7ad809d07a619e245f4d89f9eb923dce
wpetry <wpetry@users.noreply.github.com>
2017-04-13 13:31:16 +0200

    dynamic matrix size, shrinking only

--- MatrixPopModels/server.R
+++ MatrixPopModels/server.R
@@ -1,15 +1,17 @@
 # Server logic for Matrix Population Model app
 
 library(shiny)
+library(rhandsontable)
 
 # ( DF <- data.frame(matrix(0,nrow=values[["nbins"]],ncol=values[["nbins"]])))
+
+DF <- data.frame(matrix(0,nrow=10,ncol=10))
+
 
 # Define server logic to take user input matrix model and project population dynamics
 shinyServer(function(input, output) {
    
   values <- reactiveValues()
-  
-  values$DF <- data.frame(matrix(0,nrow=3,ncol=3))
   
   ## Handsontable
   observe({
@@ -27,7 +29,13 @@ shinyServer(function(input, output) {
   output$hot <- renderRHandsontable({
     DF <- values[["DF"]]
     if (!is.null(DF))
-      rhandsontable(DF, stretchH = "none")
+      if (input$nbins>1){
+        rhandsontable(DF[1:input$nbins,1:input$nbins], useTypes = F, stretchH = "none", digits = 10,
+                      colHeaders=paste0("class",1:input$nbins), rowHeaders=paste0("class",1:input$nbins)) 
+      }else{
+        rhandsontable(data.frame(X1=DF[1,1]), useTypes = F, stretchH = "none", digits = 10,
+                      colHeaders="class1", rowHeaders="class1")
+      }
   })
   
 })
--- MatrixPopModels/ui.R
+++ MatrixPopModels/ui.R
@@ -1,6 +1,7 @@
 # User-interface definition for Matrix Population Model app
 
 library(shiny)
+library(rhandsontable)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -14,14 +15,15 @@ shinyUI(fluidPage(
       helpText("Specify the structure of the matrix model using the options below."),
       h3("Set model options"),
       sliderInput(
-        "nbins",
+        inputId="nbins",
         label="Number of life cycle classes",
-        min=1L,max=10L,value=3L,step=1L,ticks=FALSE
+        min=1L,max=10L,value=10L,step=1L,ticks=FALSE
       ),
       radioButtons("matType", "Matrix type", c("Leslie (age/stage)", "Lefkovitch (size)"))
     ),
     
     mainPanel(
+      h3("Transition matrix"),
       rHandsontableOutput("hot")
     )
   )
